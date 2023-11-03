b2ea141a8a1edfe2f7d2b956d2a9a9287eda5eb2
SuperCoder <propscode254@gmail.com>
2023-11-03 08:34:02 +0100

    fix buttons

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,7 +1,7 @@
 const Button = (props) => {
   return (
     <button
-      className="btn btn-secondary m-2 font-bold font-['RobotoB'] active:border-primary active:border-4 focus:border-4 focus:border-primary"
+      className="btn btn-secondary m-2 font-bold border-4 border-transparent font-['RobotoB'] active:border-primary active:border-4 focus:border-4 focus:border-primary"
       onClick={props.action}
     >
       {props.name}
